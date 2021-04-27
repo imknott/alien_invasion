8ed91883d3cb9fdc8f9da59dcaad84714612f699
Ian Knott <ianmatknott@gmail.com>
2021-04-27 14:28:10 -0700

    commit on 'main' v-08

--- bullet.py
+++ bullet.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.sprite import Sprite
 
+
 from settings import Settings
 
 class Bullet(Sprite): 
@@ -12,6 +13,7 @@ class Bullet(Sprite):
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.color = self.settings.bullet_color
+        
 
     #create a bullet rect at (0,0) and then set correct position. 
         self.rect = pygame.Rect(0,0, self.settings.bullet_width, self.settings.bullet_height)
@@ -19,6 +21,7 @@ class Bullet(Sprite):
 
         #store the bullets position as a decimal value. 
         self.y = float(self.rect.y)
+
     
     def update(self):
         """Move the bullet up the screen."""
--- settings.py
+++ settings.py
@@ -17,3 +17,5 @@ class Settings:
         self.bullet_color = (60,60,60)
         self.bullets_allowed = 3
         
+        #Sound Settings
+        
